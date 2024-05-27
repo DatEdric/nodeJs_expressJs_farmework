35228765755d4f5054ec416c07d61e5498664ca4
Tandat <buidat027@gamil.com>
2024-05-27 14:02:18 +0700

    learning nodejs with expressJs to build a project about ECCO website

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, '/resources/public')));
 
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 app.use(morgan('combined'));
 
 app.engine('hbs', exphbs({
@@ -17,6 +23,13 @@ app.set('views',path.join(__dirname,'resources/views'));
 
 app.get('/',(req,res) => {res.render('home')});
 app.get('/news',(req,res) => {res.render('news')});
+app.get('/shops',(req,res) => {res.render('shops')});
+app.get('/product',(req,res) => {res.render('productinfo')});
+app.post('/product',(req,res) => {
+    console.log(req.body)
+    res.render('')});
+
+
 
 
 app.listen(port, () => { console.log(`Example app listening on port ${port}`)});
